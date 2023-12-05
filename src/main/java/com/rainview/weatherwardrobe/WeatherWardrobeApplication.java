2a476b53ba08d989c7623918b40fdab9e75a95e2
eumssooo <mamma1115@gmail.com>
2023-12-05 17:42:02 +0900

    :tada: create project

---
+++ src/main/java/com/rainview/weatherwardrobe/WeatherWardrobeApplication.java
@@ -0,0 +1,13 @@
+package com.rainview.weatherwardrobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherWardrobeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherWardrobeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rainview/weatherwardrobe/WeatherWardrobeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rainview.weatherwardrobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherWardrobeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
